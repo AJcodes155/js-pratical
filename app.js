7c83d50b494edade25bd2846eb9ff5ace525955c
tanjabodunrin@gmail.com <tanjabodunrin@gmail.com>
2022-08-09 15:41:51 +0100

    modified

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ let number = document.getElementById('Num')
 let dec = document.getElementById('Dec')
 let res = document.getElementById('Res')
 let inc = document.getElementById('Inc')
+let body = document.body
 
 
 function Decrease(){
     number.innerText--
+    if(number.innerText < 0){
+        body.style.backgroundColor = 'red'
+    }
 }
 
 dec.addEventListener('click',Decrease)
 
 function Reset(){
     number.innerText = 0
+    if(number.innerText ==0){
+        body.style.backgroundColor = 'yellow'
+    }
 }
 
 res.addEventListener('click',Reset)
@@ -19,6 +26,9 @@ res.addEventListener('click',Reset)
 
 function Increase(){
     number.innerText++
+    if(number.innerText > 0){
+        body.style.backgroundColor = 'green'
+    }
 }
 
 inc.addEventListener('click',Increase)
